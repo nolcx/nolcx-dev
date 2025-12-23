2a5d6f8771047d0212a59f21faa5080492f610b7
Nolan Calderon <nolan.calderon@ulatina.net>
2025-12-23 16:36:39 -0600

    feet: add new SVG icons and styles for UI components
    - Implement CVIcon, GitHubIcon, and LinkedInIcon components with SVG paths.
    - Create a glassButton style for UI elements in classes.js.
    - Define CSS variables for colors and set background styles in index.css.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,39 @@
+import profilePhotoNC from '../assets/nolancalderon.webp'
+import LinkedInIcon from '../components/icons/LinkedInIcon.jsx'
+import GitHubIcon from '../components/icons/GitHubIcon.jsx'
+import CVIcon from '../components/icons/CVIcon.jsx'
+import { glassButton } from '../styles/classes'
 import { LayoutMain } from '../layouts/layout-main'
 
 export function Home () {
   return (
     <LayoutMain>
-      <h1>Hello, this is my personal webside with React!!</h1>
+      <section className='flex justify-center items-center space-y-6 w-full p-8 gap-15'>
+        <header>
+          <h1 className='heading-1 text-3xl leading-17'>
+            <span className='block text-5xl font-bold'>Hello!</span>
+            <span className='block'>My name is <span className='text-4xl font-mono bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text font-extrabold text-transparent'>Nolan Calderón</span>,</span>
+            <span className='block'>I'm a <span className=' text-3xl font-mono bg-linear-to-r from-orange-500 to-yellow-500 bg-clip-text font-extrabold text-transparent'>Computer Systems Engineer</span>.</span>
+          </h1>
+          <div className='flex mt-6 gap-4'>
+            <a className={glassButton} href='https://github.com/nolcx'>
+              <GitHubIcon fill='#fff' width='20px' height='20px' />
+              GitHub
+            </a>
+            <a className={glassButton} href='https://www.linkedin.com/in/nolan-calderon-668313328/'>
+              <LinkedInIcon fill='#fff' width='20px' height='20px' />
+              LinkedIn
+            </a>
+            <a className={glassButton} href='https://www.linkedin.com/in/nolan-calderon-668313328/'>
+              <CVIcon fill='#fff' width='20px' height='20px' />
+              Download CV
+            </a>
+          </div>
+        </header>
+        <aside>
+          <img className='h-80 w-80 object-cover rounded-xl ' src={profilePhotoNC} alt='nolancalderon.webp' />
+        </aside>
+      </section>
     </LayoutMain>
   )
 }
---
+++ src/styles/classes.js
@@ -0,0 +1,2 @@
+const glassButton = 'rounded-full px-4 py-2 text-dark-800 dark:text-dark-200 bg-white/10 dark:bg-gray-900/30 backdrop-blur-md border border-white/10 dark:border-gray-700/30 hover:scale-105 transition-transform flex items-center gap-2'
+export { glassButton }
